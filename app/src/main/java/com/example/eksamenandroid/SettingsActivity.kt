b4d3f87951ff312bd1cd13c10500346142059ae4
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-12-09 03:02:13 +0100

    update

--- app/src/main/java/com/example/eksamenandroid/SettingsActivity.kt
+++ app/src/main/java/com/example/eksamenandroid/SettingsActivity.kt
@@ -1,12 +1,14 @@
 package com.example.eksamenandroid
 
 import android.content.ContentValues
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.EditText
+import android.widget.Spinner
 
 class SettingsActivity() : AppCompatActivity() {
 
@@ -15,6 +17,7 @@ class SettingsActivity() : AppCompatActivity() {
         setContentView(R.layout.settings)
 
         // Bruka onCreate til å kjøre testa mot functions.
+
         //insert()
         //read()
         //update()
@@ -38,6 +41,7 @@ class SettingsActivity() : AppCompatActivity() {
         db.insert("Settings", null, values)
     }
 
+    // Dinne funka sann den skal.
     fun read() {
         val db = recipesDB.readableDatabase
         val cursor = db.query("Settings", null, null, null, null, null, null, null)
@@ -55,6 +59,7 @@ class SettingsActivity() : AppCompatActivity() {
         }
     }
 
+    // Dinne funka sann den skal.
     fun update() {
         val db = recipesDB.writableDatabase
         val values = ContentValues()
@@ -65,12 +70,44 @@ class SettingsActivity() : AppCompatActivity() {
         db.update("Settings", values, "id = ?", arrayOf("1"))
     }
 
+    // Dinne funka sann den skal.
     fun delete() {
         val db = recipesDB.writableDatabase
         db.delete("Settings", "id = ?", arrayOf("1"))
     }
 
     fun saveAndReturn(view: View) {
+        val caloriesInput = findViewById<EditText>(R.id.CaloriesInput).text.toString().toInt()
+        val maxHistoryItemsInput = findViewById<EditText>(R.id.MaxHistoryItemsInput).text.toString().toInt()
+        val dietDropDown = findViewById<Spinner>(R.id.DietDropDown)
+        val cuisineDropDown = findViewById<Spinner>(R.id.CuisineDropDown)
+        val mealTypeDropDown = findViewById<Spinner>(R.id.MealTypeDropDown)
+
+        var dietInput: String? = null
+        var cuisineInput: String? = null
+        var mealTypeInput: String? = null
+
+        dietDropDown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                dietInput = parent.getItemAtPosition(position) as String
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+        cuisineDropDown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                cuisineInput = parent.getItemAtPosition(position) as String
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+        mealTypeDropDown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                mealTypeInput = parent.getItemAtPosition(position) as String
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+
+        Log.i("Current update rapport", "Calories: $caloriesInput, Max History Items: $maxHistoryItemsInput, Diet: $dietInput, Cuisine: $cuisineInput, MealType: $mealTypeInput")
+
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
