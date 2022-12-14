84e767863e0d271b0916e51aadff41b20ff0e3f7
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-12-14 22:22:31 +0100

    update

---
+++ app/src/main/java/com/example/eksamenandroid/SearchHistoryOnSelect.kt
@@ -0,0 +1,11 @@
+package com.example.eksamenandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchHistoryOnSelect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_history_onselect)
+    }
+}
